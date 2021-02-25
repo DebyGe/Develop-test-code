28bfeddee59fa494c12895a478a88dd69eb75826
DP <thedpwork@gmail.com>
2021-02-25 11:16:28 +0100

    ckeditor5-build-decoupled-document

--- Demo-2-Cli-Editor/cli-editor/src/app/cmseditor/cmseditor.component.ts
+++ Demo-2-Cli-Editor/cli-editor/src/app/cmseditor/cmseditor.component.ts
@@ -1,57 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import * as DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 
 @Component({
   selector: 'app-cmseditor',
   templateUrl: './cmseditor.component.html',
   styleUrls: ['./cmseditor.component.css']
 })
+
 export class CMSEditorComponent implements OnInit {
 
-  public Editor = ClassicEditor;  
+  public Editor = DecoupledEditor;  
+  
 
-  editorConfig = {
-    toolbar: {
-      items: [
-        'bold',
-        'italic',
-        'underline',
-        'link',
-        'bulletedList',
-        'numberedList',
-        '|',
-        'indent',
-        'outdent',
-        '|',
-        'imageUpload',
-        'blockQuote',
-        'insertTable',
-        'undo',
-        'redo',
-      ]
-    },
-    image: {
-      toolbar: [
-        'imageStyle:full',
-        'imageStyle:side',
-        '|',
-        'imageTextAlternative'
-      ]
-    },
-    table: {
-      contentToolbar: [
-        'tableColumn',
-        'tableRow',
-        'mergeTableCells'
-      ]
-    },
-    // This value must be kept in sync with the language defined in webpack.config.js.
-    language: 'en'
-  }; 
+   public onReady( editor ) {
+        editor.ui.getEditableElement().parentElement.insertBefore(
+            editor.ui.view.toolbar.element,
+            editor.ui.getEditableElement()
+        );
+
+        editor.setData( '<p><b>This is editor!</b></p>' );
+       
+    }
 
   constructor() { }
 
   ngOnInit(): void {
+   
   }
 
 }
